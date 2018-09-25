67b8ea43e62782c9f1a0f50cf0b79c4593c5222b
prasu <prai93@outlook.com>
2018-09-25 19:34:47 +0200

    completion of task 2.2

--- led_matrix_example/task2-2/task2-2.c
+++ led_matrix_example/task2-2/task2-2.c
@@ -9,7 +9,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "led_matrix.h"
 
 void sleep_ms(int milliseconds)
@@ -33,48 +34,67 @@ void pointless_calculation()
 }
 
 void run_child(int row)
-  {
+{
 
-    for( int col=0; col<6; col++)
-      {
-        pointless_calculation();
-        set_led(row, col, RGB565_GREEN);
-      }
-  }
-
+  for (int col = 0; col < 8; col++)
+    {
+      pointless_calculation();
+      printf("inside child process  (%d,%d) \n", row, col);
+      set_led(row, col, RGB565_GREEN);
+    }
+}
 
 int main()
 {
+  int n = 8;
+  pid_t pids[8];
+  int row, col;
 
   if (open_led_matrix() == -1)
     {
       printf("Failed to initialize LED matrix\n");
       return -1;
     }
-  int row, col;
+
   clear_leds();
+  nice(100);
+  /* Start children. */
+  for (int i = 0; i < n; ++i)
+    {
+      if ((pids[i] = fork()) < 0)
+        {
+          perror("fork");
+          abort();
+        }
+      else if (pids[i] == 0)
+        {   nice(i);
+          printf("\n inside child process %d  ", i);
+          run_child(i);
+          exit(0);
 
-
-for(int num_child=0;num_child<8;num_child++){
-    for(int n=0;n<num_child;n++){
-        if(fork()==0)
-          {
-            nice(n);
-            run_child(n);
-          }
-        else{
-wait(NULL);
-sleep_ms(1000);
-clear_leds();
         }
     }
-}
+
+  /* Wait for children to exit. */
+  int status;
+  pid_t pid;
+  while (n > 0)
+    {
+      pid = wait(&status);
+      printf("\n Child with PID %ld exited with status 0x%x.\n", (long) pid,
+          status);
+      --n;  // TODO(pts): Remove pid from the pids array.
+    }
+  printf("\n inside parent process and about to exit ");
+
+  clear_leds();
   if (close_led_matrix() == -1)
     {
       printf("Could not properly close LED matrix\n");
       return -1;
     }
+
   return 0;
 }
 
-
+ 
